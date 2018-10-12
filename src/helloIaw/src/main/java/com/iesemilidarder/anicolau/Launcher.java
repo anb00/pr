aa91b158fbaf70d9603761d791b2cd160c783fda
anb00 <anb00@iesemilidarder.com>
2018-10-12 08:55:00 +0200

    none

---
+++ src/helloIaw/src/main/java/com/iesemilidarder/anicolau/Launcher.java
@@ -0,0 +1,7 @@
+package com.iesemilidarder.anicolau;
+
+public  class Launcher {
+    public static void  main (String... args) {
+        System.out.println("primera clase mostramos un texto");
+    }
+}
